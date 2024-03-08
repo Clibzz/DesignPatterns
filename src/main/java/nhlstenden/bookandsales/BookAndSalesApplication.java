3c0da01be16c545abeaa8a5aee39202abb57d2f6
robin <robenjay@hotmail.nl>
2024-03-08 10:39:42 +0100

    added maven and spring

---
+++ src/main/java/nhlstenden/bookandsales/BookAndSalesApplication.java
@@ -0,0 +1,13 @@
+package nhlstenden.bookandsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAndSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAndSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhlstenden/bookandsales/BookAndSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package nhlstenden.bookandsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
