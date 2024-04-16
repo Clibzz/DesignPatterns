e7bd50ffdb64c592dcb7f1e64ba074e0f1b430d4
robin <robenjay@hotmail.nl>
2024-04-16 14:22:55 +0200

    changed style and updated all, only need to add styling to bookdetails page

--- src/main/resources/static/js/overview.js
+++ src/main/resources/static/js/overview.js
@@ -1,17 +1,4 @@
 function loadBookDetails() {
-    // Perform AJAX request
-    let xhr = new XMLHttpRequest();
     let bookId = document.getElementById("bookId").getAttribute("value");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                // Update DOM with response data
-
-            } else {
-                // Handle error
-            }
-        }
-    };
-    xhr.open("GET", "/bookDetails/" + bookId);
-    xhr.send();
+    window.location.href = "/bookDetails/" + bookId;
 }
