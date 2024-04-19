c66891004ede0f95569f0a3fd5470c3fbaf694f2
Clibzz <chriscrossje2001@gmail.com>
2024-04-19 11:43:02 +0200

    Switched Book to BookProduct in temp Main.java

--- src/main/java/nhlstenden/bookandsales/Main.java
+++ src/main/java/nhlstenden/bookandsales/Main.java
@@ -1,12 +1,14 @@
 package nhlstenden.bookandsales;
 
+import nhlstenden.bookandsales.Factory.AudioBook;
+import nhlstenden.bookandsales.Factory.BookProduct;
 import nhlstenden.bookandsales.Model.*;
 
 public class Main {
     public static void main(String[] args) {
         BookType bookType = new BookType(1, "AudioBook", "HasAutomaticReading", true);
-        Book book = new Book(1, bookType, "test", Genre.HORROR, 12, "test", "test", "TEST", 123, "test");
-        Book newBook = new Book(2, bookType, "AAAA", Genre.HORROR, 12, "AAAA", "AAAA", "AAAA", 123, "AAAA");
+        BookProduct book = new AudioBook(1, bookType, "test", 1, "test", "test", 123, Genre.HORROR, true, "test", "mario.png");
+        BookProduct newBook = new AudioBook(2, bookType, "test", 1, "test", "test", 123, Genre.HORROR, true, "test", "mario.png");
 
         PaymentCart paymentCart = new PaymentCart();
         PaymentCartHistory history = new PaymentCartHistory();
